dac7fa0bad66c296b2e9e63441c3f6c31c597c9a
Ricardo <ricardo@ricardo.(none)>
2013-01-14 01:49:09 -0300

    Se normaliza el diseño de las diferentes secciones

--- src/application/forms/DestacadoPortada.php
+++ src/application/forms/DestacadoPortada.php
@@ -1,25 +1,12 @@
 <?php
 class Application_Form_DestacadoPortada extends Zend_Form
 {
-	public function init() 
-	{
-
-		$this->removeDecorator('HtmlTag');
-		$this->removeDecorator('DtDdWrapper');
-		
-		$portada = new Zend_Form_Element_Text('id');
-		$portada->setLabel('Ingresar código (id) de la política pública o noticia');
-		$publicar = new Zend_Form_Element_Submit('publicar');
-		$publicar->setAttrib('class', 'btn');
-		$this->addElements(array($portada, $publicar));
-		$this->addDisplayGroup(
-				array('id','publicar'),
-				'destacar',
-				(array('class'=>'casitabla',"legend" => "Destacar en portada")),
-				array("HtmlTag", array("tag" => "span"))
-				);
-		
-		$this->destacar->removeDecorator('DtDdWrapper');
-		
-	}
+    public function init() 
+    {
+        $portada = new Zend_Form_Element_Text('id');
+        $portada->setLabel('Ingresar código (id) de la política pública o noticia');
+        $publicar = new Zend_Form_Element_Submit('Publicar');
+        $publicar->setAttrib('class', 'btn-primary btn-large');
+        $this->addElements(array($portada, $publicar));
+    }
 }
