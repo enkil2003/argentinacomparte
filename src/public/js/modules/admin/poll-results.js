77fed98994614a51685ed896d13c94b57fdeadf1
Ricardo <ricardo@ricardo.(none)>
2013-01-15 01:25:10 -0300

    highcharts para encuestas

--- src/public/js/modules/admin/poll-results.js
+++ src/public/js/modules/admin/poll-results.js
@@ -1,18 +1,49 @@
 var ResultsPoll={
-    loadConfirmationModal:function(modalId,o1,o2,o3,o4,r1,r2,r3,r4){
-        var $modal=jQuery('#results-'+modalId);
-        if($modal.size() === 0){
-            var modalString= '<div style="width:500px;padding:10px" id="results-'+modalId+
-                  '" class="modal hide fade">'+
-                  '<p>'+o1+' || votos:'+r1+'</p>'+
-                  '<p>'+o2+' || votos:'+r2+'</p>'+
-                  '<p>'+o3+' || votos:'+r3+'</p>'+
-                  '<p>'+o4+' || votos:'+r4+'</p>'+
-                  '</div>' ;
-            $modal=jQuery(modalString);
-            }
-        $modal.modal('show');
+    loadConfirmationModal:function(title, modalId,o1,o2,o3,o4,r1,r2,r3,r4){
+    	var chartContainer = 'results-'+modalId;
+    	var total = r1+r2+r3+r4; 
+        var chartdata = [
+                      [o1, r1*1.0],
+                      [o2, r2*1.0],
+                      [o3, r3*1.0],
+                      [o4, r4*1.0]
+                  ];
+        new Highcharts.Chart({
+            chart: {
+                renderTo: 'chartHolder',
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false
+            },
+            title: {
+                text: title
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage}%</b>',
+                percentageDecimals: 1
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        color: '#000000',
+                        connectorColor: '#000000',
+                        formatter: function() {
+                            return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
+                        }
+                    }
+                }
+            },
+            series: [{
+                type: 'pie',
+                name: title,
+                data: chartdata
+            }]
+        });
+        $modal=jQuery('#chartHolder');
+        $modal.dialog({width: '960px'});
         return false;
-        }
-
+    }
 };
